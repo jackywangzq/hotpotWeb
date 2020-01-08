0ed383b9da03724d644fd1454842cfa57886db40
jackywangzq <jackywangzq@gmail.com>
2020-01-08 17:20:20 +0800

    v2

--- src/card/card.js
+++ src/card/card.js
@@ -1,13 +1,79 @@
 import React from 'react'
 import './card.scss';
+import {Button} from 'antd-mobile';
 
-export default function card(props) {
-    let on = "visible"
-    let off = "hidden"
-    let style = {
-        color: "yellow"
+export default class card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            starNum: [0,0,0,0,0],
+            clicked: 0
+        };
+        this.score = 0;
+        this.clicked = 0;
+        this.on = "visible";
+        this.off = "hidden";
+        this.star_on ="#dd2727";
+        this.star_off = "#FFFFFF";
+        this.skin = this.skin.bind(this);
+        this.scoreValue = this.scoreValue.bind(this);
+        this.scoreValueOver =this.scoreValueOver.bind(this);
+        this.scoreValueOut =this.scoreValueOut.bind(this);
     }
-    const skin = function(e){
+
+    scoreValueOut = function(e){
+        console.log(this.clicked)
+        if(!this.clicked){
+            this.setState({
+                starNum: [0,0,0,0,0]
+            })
+        }
+        console.log(this.state.starNum)
+    }
+
+    scoreValueOver = function(e){
+        console.log(this.state.starNum)
+        console.log(e.currentTarget.getAttribute("data-index"))
+        if(e.currentTarget.getAttribute("data-index")<=1){
+            this.star_on ="#737373"
+            console.log("#737373")
+        }
+        else if(e.currentTarget.getAttribute("data-index")<4){
+            this.star_on ="#fab54d"
+            console.log("#fab54d")
+        }
+        else{
+            this.star_on ="#dd2727"
+            console.log("#dd2727")
+        }
+        for(let i=0; i<5; i++){
+            if(i<=e.currentTarget.getAttribute("data-index")){
+                this.state.starNum[i]= 1
+            }
+            else{
+                this.state.starNum[i]= 0
+            }
+        }
+        console.log(this.clicked)
+        if(this.clicked){
+            return
+        }
+        this.setState({
+            starNum: this.state.starNum
+        })
+    }
+
+    scoreValue = function(e){
+        this.clicked = 1;
+        this.setState({
+            starNum: this.state.starNum
+        })
+        this.score = e.currentTarget.getAttribute("data-index")
+        console.log(this.score)
+    }
+    
+    skin = function(e){
+        console.log(this.state.starNum[0])
         let polyline_length = document.getElementsByTagName("polyline").length;
         let j=0
         for (j; j<polyline_length; j++){
@@ -33,7 +99,8 @@ export default function card(props) {
             document.getElementsByClassName("wrapper")[0].style.backgroundColor = hex
         }
     };  
-    return (<div className="wrapper" >
+    render(){
+        return (<div className="wrapper" >
                 <div className="div1"> <img className="icon" src="https://p0.meituan.net/deal/3df7a54d32c3c852dad3e62ecb8442bf1766882.jpg%40160w_100h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D2%26y%3D2%26relative%3D1%26o%3D20"/>
                 </div>
                 <div className="div2"> 
@@ -41,7 +108,7 @@ export default function card(props) {
                         香菜牛肉 
                     </h6>
                     <p className="content">
-                        代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张张彤德莱火锅:代金券1张彤德莱火锅:代金券1彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张彤德莱火锅:代金券1张
+                        选用牛臀(牛霖)肉，取自后退近臀部的肉，臀肉(俗称米龙.黄瓜条.和尚头)，经过去筋膜，分割，切片，腌制等工序加工而成，口感滑嫩而不失肉感
                     </p>
                     <div>
                         优惠价
@@ -65,48 +132,48 @@ export default function card(props) {
                 </div>
                 <div className="toolbar" >
                     <span className="skin" >
-                        <div className="color" onClick={skin}>
+                        <div className="color" onClick={this.skin}>
                             <div>
                                 <span style={{background: '#2d555e', textAlign: 'center'}} data-index="0" >
                                     <svg data-index="0">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="0"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="0"/>
                                     </svg>
                                 </span>
                                 <span style={{background: '#3c3f43'}} data-index="1">
                                     <svg data-index="1">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="1"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="1"/>
                                     </svg>
                                 </span>
                                 <span style={{background: '#635d10'}} data-index="2">
                                     <svg data-index="2">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="2"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="2"/>
                                     </svg>
                                 </span>
                                 <span style={{background: '#345920'}} data-index="3">
                                     <svg data-index="3">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="3"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="3"/>
                                     </svg>
                                 </span>
                             </div>
                             <div>
                                 <span style={{background: '#16504b'}} data-index="4">
                                     <svg data-index="4">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="4"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="4"/>
                                     </svg>
                                 </span>
                                 <span style={{background: '#1e3a5f'}} data-index="5">
                                     <svg data-index="5">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="5"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="5"/>
                                     </svg>
                                 </span>
                                 <span style={{background: '#42275e'}} data-index="6">
                                     <svg data-index="6">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="6"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="6"/>
                                     </svg>
                                 </span>
                                 <span style={{background: '#5b2245'}} data-index="7">
                                     <svg data-index="7">  
-                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={off} data-index="7"/>
+                                        <polyline points="3,14 8,19 20,7" fill="none" stroke="red" stroke-width="3" visibility={this.off} data-index="7"/>
                                     </svg>
                                 </span>
                             </div>
@@ -114,20 +181,27 @@ export default function card(props) {
                     </span>
                     <span className="alarm">
                         <div className="alarm">
-                            <span className="icon" onClick={(e)=> {console.log(style.color) 
-                                                                   console.log(e.currentTarget.getAttribute("data-index"))}
-                                } data-index="1" style={style}></span> 
-                            <span className="icon" onClick={(e)=>console.log(e.currentTarget.getAttribute("data-index"))} data-index="2"></span> 
-                            <span className="icon" onClick={(e)=>console.log(e.currentTarget.getAttribute("data-index"))} data-index="3"></span> 
-                            <span className="icon" onClick={(e)=>console.log(e.currentTarget.getAttribute("data-index"))} data-index="4"></span> 
-                            <span className="icon" onClick={(e)=>console.log(e.currentTarget.getAttribute("data-index"))} data-index="5"></span> 
+                            <div className="star">
+                                <span className="icon" onClick={this.scoreValue} onMouseOver={this.scoreValueOver} onMouseOut={this.scoreValueOut} data-index="0" style={{color: this.state.starNum[0]?this.star_on:this.star_off}}></span> 
+                                <span className="icon" onClick={this.scoreValue} onMouseOver={this.scoreValueOver} onMouseOut={this.scoreValueOut}  data-index="1" style={{color: this.state.starNum[1]?this.star_on:this.star_off}}></span> 
+                                <span className="icon" onClick={this.scoreValue} onMouseOver={this.scoreValueOver} onMouseOut={this.scoreValueOut}  data-index="2" style={{color: this.state.starNum[2]?this.star_on:this.star_off}}></span> 
+                                <span className="icon" onClick={this.scoreValue} onMouseOver={this.scoreValueOver} onMouseOut={this.scoreValueOut}  data-index="3" style={{color: this.state.starNum[3]?this.star_on:this.star_off}}></span> 
+                                <span className="icon" onClick={this.scoreValue} onMouseOver={this.scoreValueOver} onMouseOut={this.scoreValueOut}  data-index="4" style={{color: this.state.starNum[4]?this.star_on:this.star_off}}></span> 
+                                <span className="submit">提交</span>
+                            </div>
+                            
                         </div>
                     </span>
                     <span className="more">
-                        <div className="more">
+                        <div className="more_">
                            
                         </div>
                     </span>
                 </div>
-            </div>)
+            </div>
+            
+
+            )
+    }
+    
 }
