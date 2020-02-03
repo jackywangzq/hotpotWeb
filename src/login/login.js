b74ed79e1c7a09790518f2137c85355379f95c4c
jackywangzq <jackywangzq@gmail.com>
2020-02-03 19:51:27 +0800

    v11

--- src/login/login.js
+++ src/login/login.js
@@ -1,7 +1,7 @@
 import {Input} from 'antd';
 import axios from '../axios/axios'
-import Error from './error'
-import Success from './success'
+// import Error from './error'
+// import Success from './success'
 import React, { Component } from 'react'
 import { Route, Switch, withRouter, BrowserRouter,Link} from 'react-router-dom'
 export default class login extends Component {
@@ -23,36 +23,11 @@ export default class login extends Component {
         axios('/login','post',5000,data).then(function(response){
             console.log(response)
             if(response.data.token){
+                console.log(1)
                 window.localStorage.setItem("token",response.data.token);
                 this_.props.history.push({pathname:'/index'})
             }
         })
-
-        // axios.get("http://106.52.159.25:80/login")
-        //     .then((response)=> {
-        //                         token =response.data
-        //                         console.log(response) 
-                               
-        //                         console.log(window.localStorage.getItem('txoken'))
-        //                         })
-        //     .catch(function (error) {
-        //         console.log(error);
-        //     })
-        // setTimeout(() => {
-        //     if(!token){
-        //     }  
-        //     else{
-        //     }
-         
-        // }, 1000);
-        if(!this.isLogin){
-            console.log(1)
-            this.props.history.push({pathname:'/login',state : { id : '111' }})
-        }
-        else{
-            this.props.history.push({pathname:'/main'})
-        }
-        
     }
 
     logout(){
@@ -67,10 +42,27 @@ export default class login extends Component {
                     <Input.Password placeholder="输入密码" />
                     <a onClick={this.login}>登陆</a>
                     <a onClick={this.logout}>注销</a>
-                    <div className="child-router">
-                        <Route path={`${this.props.match.path}/error`} component={Error} />
-                        <Route path={`${this.props.match.path}/Success`} component={Success} />
-                    </div>
+                <ul>
+                    <li>
+                        <Link to={'/login/success'}>success</Link>
+                    </li>
+                    <li>
+                        <Link to={'/login/error'}>error</Link>
+                    </li>
+                    <li>
+                        <Link to={'/index'}>index</Link>
+                    </li>
+                    <li onClick={()=>{
+                    console.log("返回主页")
+                    this.props.history.push({pathname:'/index'})
+                    }}>
+                        返回主页
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path={'/login/success'}>success</Route>
+                    <Route path={'/login/error'}>error</Route>
+                </Switch>
                 </div>
             </div>
         
